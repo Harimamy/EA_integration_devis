05d6f92d34564488ccd1bacc1e73e0ded067465e
Harimamy <mamyravalohery@gmail.com>
2021-09-30 17:36:16 +0300

    creating the two basis of query on sql for docentete and docligne

--- main.py
+++ main.py
@@ -32,6 +32,79 @@ def get_df_from_file_input(path):
     return df_excel_from_export
 
 
+# it's valid only on the process for inserting the devis from PROGES
+def process_docentete_df(df_export):
+    dict_docentete, client_name, do_piece, document_date, do_ref = dict(), None, None, None, None
+    for i, (index, row) in enumerate(df_export.iterrows()):
+        for col in df_export.columns:
+            if any([not document_date, pd.isna(document_date)]):
+                try:
+                    document_date = pd.to_datetime(row[col].split()[-1])
+                    print("date document -- ", document_date) if pd.notnull(document_date) else None
+                    next
+                except:
+                    print('You have an error to convert datetime from the export excel!')
+
+            elif str(row[col]).__contains__("Référence"):
+                do_ref = row[col].split(':')[1].strip()
+                print("here is do_ref", do_ref)
+
+            elif type(row[col]) == str:
+                try:
+                    if all([row[col].__contains__("nom client"), row[col].__contains__(":")]):
+                        # there is a little problem here if the first word is not the name of the client
+                        client_name = Services.add_apostrophe(row[col].split(':')[1].strip()) if client_name is None else client_name
+
+                    elif row[col].__contains__("Devis N"):
+                        do_piece = row[col].split()[-1] if do_piece is None else do_piece
+
+
+
+                except Exception as e:
+                    print("Exception catched ", e)
+                    pass
+        if all((client_name, do_piece, document_date)):
+            print("Client -- ", client_name)
+            print("No piece or No Devis -- ", do_piece)
+            print("Référence --", do_ref)
+            dict_docentete['do_date'], dict_docentete['client_name'] = document_date, client_name
+            dict_docentete['do_piece'], dict_docentete['do_ref'] = do_piece, do_ref
+            break
+    return dict_docentete
+
+
+def process_docligne_df(df_export):
+    dict_df_docligne = dict()
+    print(df_export.columns)
+    for i, (index, row) in enumerate(df_export.iterrows()):
+        index_col_first, index_col_last = None, None
+        for col in df_export.columns:
+            try:
+                if col != df_export.columns[-1]:
+                    if all((str(row[col]).__contains__('Coloris'), str(row[col]).strip().isalpha(), row[col + 1] == 'Qté')):
+                        for iter_col in range(len(df_export.columns)):
+                            if row[iter_col] == 'Coloris':
+                                index_col_first = iter_col
+                            if str(row[iter_col]) == 'P.T. TTC':
+                                index_col_last = iter_col + 1
+                                break
+                print(index_col_first, index_col_last) if index_col_first is not None and index_col_last is not None else None
+                if index_col_first is not None and index_col_last is not None:
+                    print(i, index_col_first, index_col_last)
+                    df_docligne = df_export.iloc[i:, index_col_first:index_col_last]
+                    new_header = df_docligne.iloc[0]
+                    print("on iter_col to define header of the dataframe for DL")
+                    df_docligne = df_docligne[1:]
+                    df_docligne.columns = new_header
+                    last_row = Services.cut_df_end(df_docligne)
+                    df_docligne = df_docligne.iloc[:last_row, :][[col for col in df_docligne.columns if pd.notna(col)]]
+                    print(df_docligne)
+    #                     dict_df_docligne[i] = df_docligne
+            except Exception as e:
+                raise("You have an exception catched, that's on ", e)
+    return df_docligne
+
+
 def main_process(file_path):
     z, compteur_ligne, list_not_in_article, list_not_gamme = 0, 0, [], []
     try:
@@ -500,4 +573,48 @@ def main_process(file_path):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pass
+    # the new method for writing the sql query is to define the number of field to equal with the number of the value field
+    sql_docentete = """INSERT INTO   [dbo].  [F_DOCENTETE]([DO_Domaine],  [DO_Type],  [DO_Piece],  [DO_Date],  [DO_Ref],  [DO_Tiers],  
+                        [CO_No],  [DO_Period],  [DO_Devise],  [DO_Cours],  [DE_No],  [LI_No],  [CT_NumPayeur],  [DO_Expedit],  
+                        [DO_NbFacture],  [DO_BLFact],  [DO_TxEscompte],  [DO_Reliquat],  [DO_Imprim],  [CA_Num],  [DO_Coord01],  
+                        [DO_Coord02],  [DO_Coord03],  [DO_Coord04],  [DO_Souche],  [DO_DateLivr],  [DO_Condition],  [DO_Tarif],  
+                        [DO_Colisage],  [DO_TypeColis],  [DO_Transaction],  [DO_Langue],  [DO_Ecart],  [DO_Regime],  [N_CatCompta],  
+                        [DO_Ventile],  [AB_No],  [DO_DebutAbo],  [DO_FinAbo],  [DO_DebutPeriod],  [DO_FinPeriod],  [DO_Statut],  
+                        [DO_Heure],  [CA_No],  [CO_NoCaissier],  [DO_Transfere],  [DO_Cloture],  [DO_NoWeb],  [DO_Attente],  
+                        [DO_Provenance],  [CA_NumIFRS],  [MR_No],  [DO_TypeFrais],  [DO_ValFrais],  [DO_TypeLigneFrais],  
+                        [DO_TypeFranco],  [DO_ValFranco],  [DO_TypeLigneFranco],  [DO_Taxe1],  [DO_TypeTaux1],  [DO_TypeTaxe1],  
+                        [DO_Taxe2],  [DO_TypeTaux2],  [DO_TypeTaxe2],  [DO_Taxe3],  [DO_TypeTaux3],  [DO_TypeTaxe3],  [DO_MajCpta],  
+                        [DO_Motif],  [DO_Contact],  [DO_FactureElec],  [DO_TypeTransac],  [Chantier],  [Optimisation])
+                        VALUES(0, 0, '{do_piece}', '{date_document}', '{do_ref}', '{client_name}', 
+                               0, 1, 0, 0.0, {deposit}, 0, '{client_name}', 1, 
+                               1, 0, 0.0, 0, 0, '', '', 
+                               '', '', '', 0, '1900-01-01 00:00:00', 1, 1, 
+                               1, 1, 11, 0, 0.0, 21, 1, 
+                               0, 0, '1900-01-01 00:00:00', '1900-01-01 00:00:00', '1900-01-01 00:00:00', '1900-01-01 00:00:00', 1,
+                               '{date_hour}', 0, 0, 0, 0, '', 0, 
+                               0, '', 0, 0, 0.0, 0, 
+                               0, 0.0, 0, 0.0, 0, 0,
+                               0.0, 0, 0, 0.0, 0, 0, 0, 
+                               '', '', 0, 0, '{work_site}', '{optimisation}')"""
+    unit_cost_price = 0.0
+    dl_cmup = 'NULL'
+
+    sql_docligne = """INSERT INTO [dbo].[F_DOCLIGNE] ([DO_Domaine],[DO_Type],[CT_Num],[DO_Piece],[DL_PieceBC],[DL_PieceBL],[DO_Date],
+                       [DL_DateBC],[DL_DateBL],[DL_Ligne],[DO_Ref],[DL_TNomencl],[DL_TRemPied],[DL_TRemExep],[AR_Ref],[DL_Design],
+                       [DL_Qte],[DL_QteBC],[DL_QteBL],[DL_PoidsNet],[DL_PoidsBrut],[DL_Remise01REM_Valeur],[DL_Remise01REM_Type],
+                       [DL_Remise02REM_Valeur],[DL_Remise02REM_Type],[DL_Remise03REM_Valeur],[DL_Remise03REM_Type],[DL_PrixUnitaire],
+                       [DL_PUBC],[DL_Taxe1],[DL_TypeTaux1],[DL_TypeTaxe1],[DL_Taxe2],[DL_TypeTaux2],[DL_TypeTaxe2],[CO_No],[AG_No1],
+                       [AG_No2],[DL_PrixRU],[DL_CMUP],[DL_MvtStock],[DT_No],[AF_RefFourniss],[EU_Enumere],[EU_Qte],[DL_TTC],[DE_No],
+                       [DL_NoRef],[DL_PUDevise],[DL_PUTTC],[DO_DateLivr],[CA_Num],[DL_Taxe3],[DL_TypeTaux3],[DL_TypeTaxe3],[DL_Frais],
+                       [DL_Valorise],[AR_RefCompose],[DL_NonLivre],[AC_RefClient],[DL_MontantHT],[DL_MontantTTC],[DL_FactPoids],[DL_Escompte],[DL_PiecePL],
+                       [DL_DatePL],[DL_QtePL],[DL_NoColis],[DL_NoLink],[DL_QteRessource],[DL_TypePL],[DL_DateAvancement]) 
+                       VALUES (0, 0, '{client_name}', '{do_piece}', '', '', '{date_document}', 
+                       '1900-01-01 00:00:00', '{date_document}', 10000, '{do_ref}', 0, 0, 0, '{mp_article}', '{Services.add_apostrophe(dict_AR_design[mp_article])}', 
+                       {qte}, {qte}, 0.0, 0.0, 0.0, 0.0, 1, 
+                       0.0, 0, 0.0, 0, {dl_unit_price}, 
+                       0.0, 20.0, 0, 0, 0.0, 0, 0, 0, {art_gamme_no},
+                       0, {unit_cost_price}, {dl_cmup}, 0, 0, '', '{art_eu_enumere}', {qte}, 0, {deposit},
+                       1, 0.0, {dl_pu_ttc}, '1900-01-01 00:00:00', '', 0.0, 0, 0, 0.0, 
+                       {1 if compose else 0}, {AR_ref_compose}, 0,'', {montant_ht}, 0.0, 0, 0, '', 
+                       '1900-01-01 00:00:00', {qte else 0.0}, '', 0, 0, 0, '1900-01-01 00:00:00')"""
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
