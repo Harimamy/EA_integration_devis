d26e8d6943845aed564b58030cd73159dc7f2e16
Harimamy <mamyravalohery@gmail.com>
2021-10-13 10:05:41 +0300

    commit about the the stock control on the devis pushed on the new branch

--- services/services.py
+++ services/services.py
@@ -31,7 +31,7 @@ class Services:
         df_article = pd.read_sql_query(sql='''SELECT * FROM [dbo].[F_ARTICLE]''', con=connexion)
         dict_design_article = {row['AR_Design']: str(row['AR_Ref']) for i, row in df_article.iterrows()}
         dict_article_unite = {str(row['AR_Ref']): row['AR_UniteVen'] for i, row in df_article.iterrows()}
-        return dict_design_article, dict_article_unite
+        return dict_design_article, dict_article_unite, df_article
 
     # the only error that occured is if the article is not in gamme on the file export from proges
     # and the same article is in gamme on sage
